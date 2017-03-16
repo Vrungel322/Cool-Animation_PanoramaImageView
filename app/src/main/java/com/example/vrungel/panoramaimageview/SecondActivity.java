ca8a9ff9f6734b0026b94b0897b6cd0fd20f5fa6
Vrungel <nikita241296@gmail.com>
2017-03-16 17:16:59 +0200

    add animation back to parent activity

--- app/src/main/java/com/example/vrungel/panoramaimageview/SecondActivity.java
+++ app/src/main/java/com/example/vrungel/panoramaimageview/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.vrungel.panoramaimageview;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -16,5 +17,17 @@ public class SecondActivity extends AppCompatActivity {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
       mtvMoreText.setTransitionName("transitionName");
     }
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+          finishAfterTransition();
+        }
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
   }
 }
